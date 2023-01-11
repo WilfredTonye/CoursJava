2a3e9a33464dc6272f401a1970a5c27c5831cb06
wilfredtonye003@gmail.com <wilfredtonye003@gmail.com>
2023-01-11 16:49:04 +0100

    Mes debut en Java et creation de mon premier objet

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        FirstClass Objet1 = new FirstClass();// creation de l'objet Objet1
+        System.out.println(Objet1.x);//affichage de la valeur de la variable x a travers l'objet
+    }
+}
