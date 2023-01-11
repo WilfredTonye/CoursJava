09598507e5bc577c44889fde20b1ebe6da927a09
wilfredtonye003@gmail.com <wilfredtonye003@gmail.com>
2023-01-11 17:25:24 +0100

    Les attribut en Java

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,3 +1,6 @@
 public class FirstClass {
     int x =5;
+    String nom = "Tonye";
+    String prenom = "Wilfred";
+    int age = 20;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 public class Main {
+   int x = 82;
+    int y = 25;
     public static void main(String[] args) {
         System.out.println("Hello world!");
         FirstClass Objet1 = new FirstClass();// creation de l'objet Objet1
         System.out.println(Objet1.x);//affichage de la valeur de la variable x a travers l'objet
+
+        //Les Attributs java
+        Main chiffre = new Main();
+        System.out.println(chiffre.x);
+        //Modification de la valeur
+        Main essai1 = new Main();
+        essai1.x = 40;
+        System.out.println(essai1.x);
+        //Remplacement de la valeur
+        Main essai2 = new Main();
+        essai2.y =75;
+        System.out.println(essai2.y);
+        FirstClass pres = new FirstClass();
+        System.out.println("Je m'appelle:"+" "+ pres.prenom+pres.nom+" "+"je suis agee de:"+" "+pres.age+"ans");
     }
 }
