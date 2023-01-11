8457e42844ea173e75fd5a1e3320867274071ed6
wilfredtonye003@gmail.com <wilfredtonye003@gmail.com>
2023-01-11 20:10:34 +0100

    Les methodes en Java

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Mes debut en Java
 #1Creation d'un objet en java
 
 #2 creation d'un attribut en java
+
+#3 creation de methode en java
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
-   int x = 82;
+    int x = 82;
     int y = 25;
+    // difference entre la methode static et la methode public
+
+    //Static method
+      static void methodestatic() {
+        System.out.println("Static methods can be called without creating objects");
+}
+     // Public method
+    public void methodepublic(){
+        System.out.println("Public methods must be called by creating objects");
+    }
+
+    static void Bonjour() {//creation de la methode Bonjour
+        System.out.println("Bonjout a tous");
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
         FirstClass Objet1 = new FirstClass();// creation de l'objet Objet1
@@ -19,5 +33,11 @@ public class Main {
         System.out.println(essai2.y);
         FirstClass pres = new FirstClass();
         System.out.println("Je m'appelle:"+" "+ pres.prenom+pres.nom+" "+"je suis agee de:"+" "+pres.age+"ans");
+        Bonjour();//appelle de la methode Bonjour
+        methodestatic(); // Call the static method
+        // myPublicMethod(); This would compile an error
+
+        Main myObj = new Main(); // Create an object of Main
+        myObj.methodepublic(); // Call the public method on the object
     }
 }
